d9e010b5529cd3c488878b4380ed222fccb92e5e
Agastya Kommanamanchi <kommanamanchi.agastya@gmail.com>
2022-06-23 15:52:14 -0400

    Create postgresql-sp.sql

---
+++ postgresql-sp.sql
@@ -0,0 +1,20 @@
+CREATE PROCEDURE ins_emp (numeric, varchar(100), bool, date, float8, money, text)
+
+LANGUAGE 'plpgsql'
+
+AS $$
+
+BEGIN 
+
+CREATE TABLE IF NOT EXISTS emp(
+  id numeric,
+  fName varchar(100), 
+  lName varchar(100), 
+  isEmployed bool, 
+  hireDate date, 
+  rating float8, 
+  salary money, 
+  description text)
+
+INSERT INTO emp(id, fName, lName, isEmployed, hireDate, rating, salary, description)
+VALUES ($1, $2, $3, $4,
