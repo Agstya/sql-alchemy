2de69083b94bdd6f0049e2eea9b3239ebc8b073a
Agastya Kommanamanchi <kommanamanchi.agastya@gmail.com>
2022-06-23 16:35:45 -0400

    Update postgresql-sp.sql

--- postgresql-sp.sql
+++ postgresql-sp.sql
@@ -1,13 +1,8 @@
 CREATE SCHEMA deworktest;
 
-CREATE PROCEDURE ins_emp (numeric, varchar(100), varchar(100), bool, date, float8, float8, text)
-
-LANGUAGE 'plpgsql'
-
+CREATE OR REPLACE PROCEDURE ins_emp (numeric, varchar(100), varchar(100), bool, date, float8, float8, text)
 AS $$
-
 BEGIN 
-
 CREATE TABLE IF NOT EXISTS deworktest.emp(
   id numeric,
   fName varchar(100), 
@@ -17,12 +12,8 @@ CREATE TABLE IF NOT EXISTS deworktest.emp(
   rating float8, 
   salary float8, 
   description text);
-
 INSERT INTO deworktest.emp(id, fName, lName, isEmployed, hireDate, rating, salary, description)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
-
 COMMIT;
-
 END;
-
-$$
+$$ LANGUAGE 'plpgsql'
